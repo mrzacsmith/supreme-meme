29d9f64f3e952fa1108603fc452eaced311a3305
Zac Smith <zrs3141592@gmail.com>
2017-04-10 15:48:22 -0600

    Final site with color selector

--- js/main.js
+++ js/main.js
@@ -1,12 +1,35 @@
+var newItemCounter = 1;
+var ourList = document.querySelector("#list-items");
+
+// Can replace getElementById with querySelector for modern browswers
+// var ourList = document.getElementById("list-items");
+
+var ourButton = document.getElementById("add-new-item");
 var ourHeadline = document.getElementById("our-headline");
+
 var listItems = document.getElementById("list-items").getElementsByTagName("li");
 
-for (var i = 0; i < listItems.length; i++) {
-    // listItems[i].innerHTML = "Hello World";
-    listItems[i].addEventListener("click", activateItem);
+// for (var i = 0; i < listItems.length; i++) {
+//     // listItems[i].innerHTML = "Hello World";
+//     listItems[i].addEventListener("click", activateItem);
+// }
+
+ourList.addEventListener("click", activateItem);
+
+function activateItem (e) {
+  // alert("Click detected!");
+  if (e.target.nodeName == "LI") {
+    ourHeadline.innerHTML = e.target.innerHTML;
+    for (var i = 0; i < e.target.parentNode.children.length; i++) {
+      e.target.parentNode.children[i].classList.remove("active");
+    }
+    e.target.classList.add("active");
+  }
 }
 
-function activateItem () {
-  // alert("Click detected!");
-  ourHeadline.innerHTML = this.innerHTML;
+ourButton.addEventListener("click", createNewItem);
+
+function createNewItem () {
+  ourList.innerHTML += "<li>Something new " + newItemCounter + "</li>" ;
+  newItemCounter++;
 }
